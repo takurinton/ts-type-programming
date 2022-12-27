6f2b5e4f489b47eaec81fa3b659617fb0073c918
takurinton <takurinton@takurinton.dev>
2022-12-27 17:35:38 +0900

    add: type JudgeFizzBuzz

--- index.ts
+++ index.ts
@@ -27,21 +27,28 @@ type IsDivisible15<T extends number> = IsDivisible3<T> extends true &
   ? true
   : false;
 
+type JudgeFizzBuzz<N extends number> = IsDivisible15<N> extends true
+  ? "fizzbuzz"
+  : IsDivisible3<N> extends true
+  ? "fizz"
+  : IsDivisible5<N> extends true
+  ? "buzz"
+  : N;
+
 type FizzBuzz<N extends number, R extends any[] = []> = R["length"] extends N
   ? R
-  : FizzBuzz<
-      N,
-      [
-        ...R,
-        IsDivisible15<Add<R["length"], 1>> extends true
-          ? "fizzbuzz"
-          : IsDivisible3<Add<R["length"], 1>> extends true
-          ? "fizz"
-          : IsDivisible5<Add<R["length"], 1>> extends true
-          ? "buzz"
-          : Add<R["length"], 1>
-      ]
-    >;
+  : FizzBuzz<N, [...R, JudgeFizzBuzz<Add<R["length"], 1>>]>;
 
-// [1, 2, 'fizz', 4, 'buzz', ..., 14, 'fizzbuzz']
-let fizzbuzz: FizzBuzz<15>;
+// 1
+let num1: JudgeFizzBuzz<1>;
+// 2
+let num2: JudgeFizzBuzz<2>;
+// "fizz"
+let fizz: JudgeFizzBuzz<3>;
+// "buzz"
+let buzz: JudgeFizzBuzz<5>;
+// "fizzbuzz"
+let fizzbuzz: JudgeFizzBuzz<15>;
+
+// [1, 2, "fizz", 4, "buzz", "fizz", 7, 8, "fizz", "buzz", 11, "fizz", 13, 14, "fizzbuzz"]
+let fizzbuzzList: FizzBuzz<15>;
