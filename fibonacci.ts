b6174c07fab390abe9422231be63fd8dbe57bbf3
takurinton <takurinton@takurinton.dev>
2022-12-28 19:21:14 +0900

    fix

--- fibonacci.ts
+++ fibonacci.ts
@@ -5,10 +5,6 @@ type Fibonacci<
   R extends any[] = [0, 1]
 > = R["length"] extends N
   ? R
-  : N extends 0
-  ? Fibonacci<1>
-  : N extends 1
-  ? Fibonacci<2>
   : Fibonacci<N, [...R, Add<R[Sub<R["length"], 1>], R[Sub<R["length"], 2>]>]>;
 
 // [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
